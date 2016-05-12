2bf4cf9756366cb4e6566845b1e8fcd36fd95af5
Tim Mendenhall <timmendenhall@gmail.com>
2016-05-12 10:28:11 -0400

    adding sass build back to gulp, license, some dependencies

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -8,43 +8,52 @@ const plugins = loadPlugins();
 import componentsWebpackConfig from './webpack.config';
 
 gulp.task('components-js', ['clean'], (cb) => {
-  webpack(componentsWebpackConfig, (err, stats) => {
-    if(err) throw new plugins.util.PluginError('webpack', err);
+    webpack(componentsWebpackConfig, (err, stats) => {
+        if(err) throw new plugins.util.PluginError('webpack', err);
 
-    plugins.util.log('[webpack]', stats.toString());
+        plugins.util.log('[webpack]', stats.toString());
 
-    cb();
-  });
+        cb();
+    });
 });
 
-// gulp.task('sass', function () {
-//     return gulp.src('src/styles#<{(||)}>#*.scss')
-//                .pipe(plugins.sass().on('error', plugins.sass.logError))
-//                .pipe(gulp.dest('./dist'));
-// });
-//
-// gulp.task('autoprefixer', ['sass'], function () {
-//     return gulp.src('./dist/styles.css')
-//                .pipe(plugins.autoprefixer({browsers: ['last 2 versions']}))
-//                .pipe(gulp.dest('./dist'));
-// });
-//
-// gulp.task('minify-css', ['autoprefixer'], function () {
-//     return gulp.src('./dist/styles.css')
-//                .pipe(plugins.rename({suffix: '.min'}))
-//                .pipe(plugins.cleanCss())
-//                .pipe(gulp.dest('./dist'))
-//                .pipe(plugins.livereload());
-// });
+gulp.task('copy-package-json', ['clean'], () => {
+    return gulp.src('package.json')
+    .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('copy-license', ['clean'], () => {
+    return gulp.src('LICENSE')
+    .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('sass', function () {
+    return gulp.src('src/styles/**/*.scss')
+               .pipe(plugins.sass().on('error', plugins.sass.logError))
+               .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('autoprefixer', ['sass'], function () {
+    return gulp.src('./dist/styles.css')
+               .pipe(plugins.autoprefixer({browsers: ['last 2 versions']}))
+               .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('minify-css', ['autoprefixer'], function () {
+    return gulp.src('./dist/styles.css')
+               .pipe(plugins.rename({suffix: '.min'}))
+               .pipe(plugins.cleanCss())
+               .pipe(gulp.dest('./dist'));
+});
 
 gulp.task('clean', (cb) => {
-  rimraf('./dist', cb);
+    rimraf('./dist', cb);
 });
 
-gulp.task('build', ['clean', 'components-js']);
+gulp.task('build', ['clean', 'copy-package-json', 'copy-license', 'components-js', 'sass', 'autoprefixer', 'minify-css']);
 
 gulp.task('watch', ['default'], () => {
-    // gulp.watch('src/styles#<{(||)}>#*.scss', ['sass', 'autoprefixer', 'minify-css']);
+    gulp.watch('src/styles/**/*.scss', ['build']);
     gulp.watch('src/components/**/*.js', ['build']);
 });
 

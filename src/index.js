40fccb6c52e14194df77a31d233ebdbe1a747d18
Tony Mamo <tony@tonymamo.com>
2016-05-18 15:54:30 -0400

    new Loading component

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import Button from './components/Button/Button.js';
 import Input from './components/Input/Input.js';
 import Alert from './components/Alert/Alert.js';
+import Loading from './components/Loading/Loading.js';
 
 export { Button };
 export { Input };
 export { Alert };
+export { Loading };

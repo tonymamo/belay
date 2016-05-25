38bb6fd90f649df244c8a6f187c56f2a94eb39be
Tim Mendenhall <timmendenhall@gmail.com>
2016-05-25 12:15:26 -0400

    Adding email validation and validations folder to belay

---
+++ src/validations/email.js
@@ -0,0 +1,3 @@
+export const isEmail = (email) => {
+    return /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(email);
+};
---
+++ src/validations/validations.js
@@ -0,0 +1,7 @@
+import {isEmail} from './email.js';
+
+const validations = {
+    isEmail
+};
+
+export default validations;

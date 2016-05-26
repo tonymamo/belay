1a4af817dc98e88cf1a2ac3ffe8e1490c6c65e6a
Tony Mamo <tony@tonymamo.com>
2016-05-26 11:28:17 -0400

    remove unneeded class name

--- src/components/Loading/Loading.js
+++ src/components/Loading/Loading.js
@@ -8,7 +8,7 @@ export default class Loading extends Component {
     render() {
         return (
             <div className={this.props.fullScreen ? 'loading loading--fullscreen' : 'loading'}>
-                <svg width='32px' height='32px' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" preserveAspectRatio="xMinYMin" class="uil-comment">
+                <svg width='32px' height='32px' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" preserveAspectRatio="xMinYMin">
                     <path d="M78,19H22c-6.6,0-12,5.4-12,12v31c0,6.6,5.4,12,12,12h37.2c0.4,3,1.8,5.6,3.7,7.6c2.4,2.5,5.1,4.1,9.1,4 c-1.4-2.1-2-7.2-2-10.3c0-0.4,0-0.8,0-1.3h8c6.6,0,12-5.4,12-12V31C90,24.4,84.6,19,78,19z" className="loading__bubble"></path>
                     <circle cx="30" cy="47" r="5" className="loading__dot" />
                     <circle cx="50" cy="47" r="5" className="loading__dot" />

a7ea7c61c3607e599258405f701dfdb01781003e
Tony Mamo <tony@tonymamo.com>
2016-06-10 21:14:51 -0400

    change to container fluid class

--- src/components/Grid/Container.js
+++ src/components/Grid/Container.js
@@ -3,7 +3,7 @@ import React from 'react';
 class Container extends React.Component {
     render() {
         return (
-            <div className="container">
+            <div className="container-fluid">
                 {this.props.children}
             </div>
         );

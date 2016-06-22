8e4ae934228491bcc29e2d3c6111a5962caf0fce
Tony Mamo <tony@tonymamo.com>
2016-06-22 09:42:17 -0400

    add react-router, refactor Button to use button or Link with a type case

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,4 +1,5 @@
 import React, { Component, PropTypes } from 'react';
+import { Link } from 'react-router';
 import classNames from 'classnames';
 
 class Button extends Component {
@@ -7,10 +8,12 @@ class Button extends Component {
     }
 
     render() {
-        let {color, icon, text, disabled, block, size, className} = this.props;
+        let {type, to, color, icon, text, disabled, block, size, className} = this.props;
+        let content;
 
         var classList = classNames(
             'button',
+            'text--truncate',
             `button--${color}`,
             {
                 'button--block': block,
@@ -19,18 +22,34 @@ class Button extends Component {
             }
         );
 
-        return (
-            <button {...this.props} className={classList} title={text} disabled={disabled}>
-                {icon && <span className={`icon icon-${icon}`}></span>}
-                {text}
-            </button>
-        );
+        switch (type.toLowerCase()) {
+            case 'button':
+                content =   <button {...this.props} className={classList} title={text} disabled={disabled}>
+                                {icon && <span className={`icon icon-${icon}`}/>}
+                                {text}
+                            </button>;
+                break;
+            case 'link':
+                content =   <Link {...this.props} className={classList} title={text}>
+                                {icon && <span className={`icon icon-${icon}`}/>}
+                                {text}
+                            </Link>;
+                break;
+            default:
+                content =   <button {...this.props} className={classList} title={text} disabled={disabled}>
+                                {icon && <span className={`icon icon-${icon}`}/>}
+                                {text}
+                            </button>;
+                break;
+        }
+
+        return content;
     }
 }
 
 Button.propTypes = {
-    type: PropTypes.string, // TODO: a switch for toggling between <button> and <Link>
-    color: PropTypes.string.isRequired,
+    type: PropTypes.string.isRequired,
+    color: PropTypes.string,
     icon: PropTypes.string,
     text: PropTypes.string.isRequired,
     disabled: PropTypes.bool,

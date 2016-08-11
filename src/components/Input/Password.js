327ed87916e8ede17250840c9e992b20e61758fd
Tony Mamo <tony@tonymamo.com>
2016-08-10 21:51:16 -0400

    only show the toggle button if field has some value to it. Testing out getting rid of z-index on input group control for validation icon to show up right

--- src/components/Input/Password.js
+++ src/components/Input/Password.js
@@ -21,15 +21,17 @@ class Password extends Component {
         return (
             <div className="input-group">
                 <input {...this.props} {...this.props.field} type={ revealed ? 'text' : 'password'} className="form-control"/>
-                <span className="input-group__button">
-                    <button 
-                        className={`button button--${revealed ? 'danger' : 'success'}`}
-                        type="button" 
-                        onClick={this.toggleVisibility.bind(this)}
-                        title={ revealed ? 'Hide Password' : 'Show Password'}>
-                            <small className="text--uppercase">{revealed ? 'Hide' : 'Show'}</small>
-                    </button>
-                </span>
+                { field.value != '' &&
+                    <span className="input-group__button">
+                        <button 
+                            className={`button button--${revealed ? 'danger' : 'success'}`}
+                            type="button" 
+                            onClick={this.toggleVisibility.bind(this)}
+                            title={ revealed ? 'Hide Password' : 'Show Password'}>
+                                <small className="text--uppercase">{revealed ? 'Hide' : 'Show'}</small>
+                        </button>
+                    </span> 
+                }
             </div>
         );
     }

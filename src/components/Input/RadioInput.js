a0b85e08959a38a98f46892161c18c0ee89f4abd
Kevin Shreve <kevinshreve@flashrecruit.com>
2016-09-01 14:39:37 -0400

    Freeform text field now is an option for radiogroupOptions. Just need to pass option.Type="text".

--- src/components/Input/RadioInput.js
+++ src/components/Input/RadioInput.js
@@ -3,11 +3,14 @@ import React, { Component, PropTypes } from 'react';
 class RadioInput extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            freeformSelected: false
+        };
     }
 
     componentDidMount() {
         // select the first item if it's available and nothing is selected yet
-        if (this.props.field && !this.props.field.value && this.props.options && this.props.options.length > 0) {
+        if ( this.props.field && !this.props.field.value && this.props.options && this.props.options.length > 0 ) {
             this.props.field.onChange(this.props.options[0].Key.toString());
         }
     }
@@ -23,16 +26,52 @@ class RadioInput extends Component {
         return renderedOptions;
     }
 
+    freeformLabelClick() {
+        // Set the field value to the ref. This lets the value be set if a user just clicks the radio button or label
+        this.props.field.onChange(this.freeform.value);
+        this.setState({
+            freeformSelected: true
+        });
+    }
+
+    freeformChange(e) {
+        e.stopPropagation();
+        this.setState({
+            freeformSelected: true
+        });
+
+        this.props.field.onFocus();
+        // have to grab the value via ref because we could be clicking on radio, label, or input
+        this.props.field.onChange(this.freeform.value);
+        return this.props.field.onBlur();
+    }
+
     renderRadioOption(option) {
-        const {inline, field: {value}} = this.props;
-        let keyValue = option.Key.toString();
+        const { inline, field } = this.props;
+        let { value, label } = field,
+            key = option.Key.toString();
 
         return (
-            <div className={`${inline ? 'radio radio--inline' : 'radio'}`} key={keyValue}>
-                <label title={option.Value}>
-                    <input type="radio" {...this.props} {...this.props.field} value={keyValue} checked={value === keyValue}/>
-                    <span>{option.Value}</span>
-                </label>
+            <div className={`${inline ? 'radio radio--inline' : 'radio'}`} key={key}>
+                {
+                    !option.Type &&
+                    <label title={option.Value} onClick={() => this.setState({ freeformSelected: false })}>
+                        <input type="radio" {...this.props} {...this.props.field} value={key} checked={value === key}/>
+                        <span>{option.Value}</span>
+                    </label>
+                }
+                {
+                    option.Type && option.Type.toLowerCase() === 'text' &&
+                    <label title={option.Value} onClick={this.freeformLabelClick.bind(this)}>
+                        <input type="radio" value={key} checked={this.state.freeformSelected}/>
+                        <input maxLength="30"
+                               ref={(input) => this.freeform = input}
+                               onClick={this.freeformChange.bind(this)}
+                               onChange={this.freeformChange.bind(this)}
+                               onBlur={this.freeformChange.bind(this)}
+                               className="form-control"/>
+                    </label>
+                }
             </div>
         );
     }
